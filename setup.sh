599a28fde0f64ada80dfc00d15e59aa0d556788d
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2021-09-27 22:25:54 +0900

    Fix: change homebrew install command

--- setup.sh
+++ setup.sh
@@ -34,7 +34,7 @@ done
 
 if ! command -v brew >/dev/null 2>&1; then
     # Install homebrew: https://brew.sh/
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     echo
 fi
 brew bundle
