bd5202e5bfc3db5ede496b954033f3546a2d8974
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2021-09-27 22:17:01 +0900

    Add: add script for install homebrew and Brewfile

---
+++ setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -eu
+
+THIS_DIR=$HOME/dotfiles
+
+if [ ! -d "$THIS_DIR" ]; then
+    git clone https://github.com/Kiyo510/kiyo_dotfile.git "$THIS_DIR"
+else
+    echo "$THIS_DIR already downloaded. Updating..."
+    cd "$THIS_DIR"
+    git stash
+    git checkout master
+    git pull origin master
+    echo
+fi
+
+cd $THIS_DIR
+
+echo "start setup..."
+
+for f in .??*; do
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" = ".gitconfig.local.template" ]] && continue
+    [[ "$f" = ".gitmodules" ]] && continue
+    [[ "$f" = ".gitignore" ]] && continue
+    [[ "$f" == ".DS_Store" ]] && continue
+    [[ "$f" == ".aws" ]] && continue
+    [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".ssh" ]] && continue
+
+    ln -snfv "$THIS_DIR/$f" ~/
+done
+
+if ! command -v brew >/dev/null 2>&1; then
+    # Install homebrew: https://brew.sh/
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    echo
+fi
+brew bundle
+echo
+
+cat <<END
+**************************************************
+DOTFILES SETUP FINISHED! bye.
+**************************************************
+END
