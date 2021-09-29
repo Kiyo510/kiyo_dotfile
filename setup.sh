b0774d7eb748ce64df2e175f931a887757c46880
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2021-09-30 00:58:17 +0900

    Fix: change place defition of function

--- setup.sh
+++ setup.sh
@@ -41,7 +41,6 @@ fi
 brew bundle
 
 # setup Prezto
-./install_zprezto.sh
 set_prezto
 
 # install z
@@ -51,6 +50,38 @@ if ! command -v z >/dev/null 2>&1; then
 else
     echo 'z already installed.'
 fi
+
+set_prezto() {
+    if [ ! -d "$HOME/.zprezto" ]; then
+        git clone --recursive \
+            https://github.com/sorin-ionescu/prezto.git \
+            $HOME/.zprezto
+    else
+        echo "$HOME/.zprezto already exists"
+    fi
+
+    echo "~/.zprezto/runcomsにある"
+    echo "zsh系の設定ファイルを一旦削除"
+
+    cd ~/.zprezto/runcoms
+    rm -rf z*
+
+    echo "~/.zprezto/runcomsに"
+    echo ".z系のシンボリックリンクを貼る"
+    echo "本来なら~/.zprezto/runcoms側から貼るけど"
+    echo "dotfiles管理のために逆から貼る"
+
+    ln -s $HOME/.zlogin $HOME/.zprezto/runcoms/zlogin &&
+        ln -s $HOME/.zlogout $HOME/.zprezto/runcoms/zlogout &&
+        ln -s $HOME/.zpreztorc $HOME/.zprezto/runcoms/zpreztorc &&
+        ln -s $HOME/.zprofile $HOME/.zprezto/runcoms/zprofile &&
+        ln -s $HOME/.zshenv $HOME/.zprezto/runcoms/zshenv &&
+        ln -s $HOME/.zshrc $HOME/.zprezto/runcoms/zshrc
+
+    echo "######################################"
+    echo "Success!"
+    echo "######################################"
+}
 
 echo
 
