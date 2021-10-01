a4fd1454339af2e068e1aa2721622ab45387d397
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2021-10-01 22:45:52 +0900

    Refactor: Delete unnecessary explanations

--- install_zprezto.sh
+++ install_zprezto.sh
@@ -11,24 +11,18 @@ set_prezto() {
         echo "$HOME/.zprezto already exists"
     fi
 
-    echo "~/.zprezto/runcomsにある"
     echo "zsh系の設定ファイルを一旦削除"
-
     cd ~/.zprezto/runcoms
     rm -rf z*
 
-    echo "~/.zprezto/runcomsに"
-    echo ".z系のシンボリックリンクを貼る"
     echo "本来なら~/.zprezto/runcoms側から貼るけど"
     echo "dotfiles管理のために逆から貼る"
-
     ln -s $HOME/.zlogin $HOME/.zprezto/runcoms/zlogin &&
         ln -s $HOME/.zlogout $HOME/.zprezto/runcoms/zlogout &&
         ln -s $HOME/.zpreztorc $HOME/.zprezto/runcoms/zpreztorc &&
         ln -s $HOME/.zprofile $HOME/.zprezto/runcoms/zprofile &&
         ln -s $HOME/.zshenv $HOME/.zprezto/runcoms/zshenv &&
         ln -s $HOME/.zshrc $HOME/.zprezto/runcoms/zshrc
-
     echo "######################################"
     echo "Success!"
     echo "######################################"
