f70fa6cdb2fe432d70d601f6e248e078b962bf11
sano <sano@maebe.jp>
2021-10-04 13:25:36 +0900

    Fix: vscode setting condition

--- vscode/sync.sh
+++ vscode/sync.sh
@@ -5,16 +5,16 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
 
 echo "start sync vscode.."
-# if [ ! -d "$VSCODE_SETTING_DIR" ]; then
-#      mkdir -p "$VSCODE_SETTING_DIR"
-# fi
+if [ ! -d "$VSCODE_SETTING_DIR" ]; then
+     mkdir -p "$VSCODE_SETTING_DIR"
+fi
 
-if [ -e "${VSCODE_SETTING_DIR}/settings.json" ]; then
+if [ -L "${VSCODE_SETTING_DIR}/settings.json" ]; then
     rm "${VSCODE_SETTING_DIR}/settings.json"
 fi
 ln -s "${SCRIPT_DIR}/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
 
-if [ -e "${VSCODE_SETTING_DIR}/keybindings.json" ]; then
+if [ -L "${VSCODE_SETTING_DIR}/keybindings.json" ]; then
     rm "${VSCODE_SETTING_DIR}/keybindings.json"
 fi
 ln -s "${SCRIPT_DIR}/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
