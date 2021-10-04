524ff61bd4f50cb6c8f1e2b54578c68b7bc28d0f
sano <sano@maebe.jp>
2021-10-04 14:48:10 +0900

    Fix: check vscode setting condition

--- vscode/sync.sh
+++ vscode/sync.sh
@@ -3,19 +3,20 @@ set -exu
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 VSCODE_SETTING_DIR=~/Library/Application\ Support/Code/User
+DATE=$(date +%s)
 
 echo "start sync vscode.."
 if [ ! -d "$VSCODE_SETTING_DIR" ]; then
      mkdir -p "$VSCODE_SETTING_DIR"
 fi
 
-if [ -L "${VSCODE_SETTING_DIR}/settings.json" -o -e "${VSCODE_SETTING_DIR}/settings.json" ]; then
-    rm "${VSCODE_SETTING_DIR}/settings.json"
+if [ -L "${VSCODE_SETTING_DIR}/settings.json" -o -f "${VSCODE_SETTING_DIR}/settings.json" ]; then
+    mv "${VSCODE_SETTING_DIR}/settings.json" "${VSCODE_SETTING_DIR}/settings_$DATE.json"
 fi
 ln -s "${SCRIPT_DIR}/settings.json" "${VSCODE_SETTING_DIR}/settings.json"
 
-if [ -L "${VSCODE_SETTING_DIR}/keybindings.json" -o -e "${VSCODE_SETTING_DIR}/keybindings.json" ]; then
-    rm "${VSCODE_SETTING_DIR}/keybindings.json"
+if [ -L "${VSCODE_SETTING_DIR}/keybindings.json" -o -f "${VSCODE_SETTING_DIR}/keybindings.json" ]; then
+    mv "${VSCODE_SETTING_DIR}/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings_$DATE.json"
 fi
 ln -s "${SCRIPT_DIR}/keybindings.json" "${VSCODE_SETTING_DIR}/keybindings.json"
 
