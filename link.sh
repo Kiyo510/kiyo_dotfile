d981612dea88f7cae6b9356aed811f33066f72ce
Kiyo510 <kanzenziai_r@yahoo.co.jp>
2021-10-09 15:39:21 +0900

    Update: Adjusted the conditions for deploying

--- link.sh
+++ link.sh
@@ -8,15 +8,15 @@ ZSH_DIR=$THIS_DIR/zsh/
 echo "deploy starting..."
 for f in .??*; do
     [[ "$f" == ".git" ]] && continue
+    [[ "$f" == ".github" ]] && continue
+    [[ "$f" == ".gitignore" ]] && continue
     [[ "$f" == ".gitconfig.local.template" ]] && continue
-    [[ "$f" =~ .ssh|.aws|.DS_Store|.gitignore|.gitmodules ]] && continue
+    [[ "$f" =~ .ssh|.aws|.DS_Store|.gitmodules ]] && continue
     ln -snfv "$THIS_DIR/$f" ~/
 done
 
 cd $ZSH_DIR
 for f in .??*; do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".gitconfig.local.template" ]] && continue
     [[ "$f" =~ .ssh|.aws|.DS_Store|.gitignore|.gitmodules ]] && continue
     ln -snfv "$ZSH_DIR/$f" ~/
 done
