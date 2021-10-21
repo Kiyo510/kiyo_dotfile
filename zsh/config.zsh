f6b104b57032e810e079734bbdbe6853c36c9631
sano <sano@maebe.jp>
2021-10-21 18:22:45 +0900

    Fix: vscode tarminal process failed

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -87,14 +87,17 @@ function mm() {
     fi
 }
 
-##### 補完関連 #######
-fpath=($(brew --prefix)/share/zsh/site-functions $fpath)
-# 補完機能有効にする
-autoload -U compinit
-compinit -u
+# ##### 補完関連 #######
+zinit ice wait'0'
+zinit light zsh-users/zsh-completions
+autoload -Uz compinit && compinit
+## 補完で小文字でも大文字にマッチさせる
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+## 補完候補を一覧表示したとき、Tabや矢印で選択できるようにする
+zstyle ':completion:*:default' menu select=1
 # 補完候補に色つける
 autoload -U colors
-colors
+# colors
 zstyle ':completion:*' list-colors "${LS_COLORS}"
 # 単語の入力途中でもTab補完を有効化
 setopt complete_in_word
@@ -102,11 +105,6 @@ setopt complete_in_word
 zstyle ':completion:*:default' menu select=1
 # キャッシュの利用による補完の高速化
 zstyle ':completion::complete:*' use-cache true
-# 大文字、小文字を区別せず補完する
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 # 補完リストの表示間隔を狭くする
 setopt list_packed
-# コマンドの打ち間違いを指摘してくれる
-setopt correct
-SPROMPT="correct: $RED%R$DEFAULT -> $GREEN%r$DEFAULT ? [Yes/No/Abort/Edit] => "
-##### 補完関連 #######
+# ##### 補完関連 #######
