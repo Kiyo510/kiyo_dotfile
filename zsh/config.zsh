399efcea48c25bdb77ed5bcef30169e2cd747fbd
Kiyo510 <miraishida00510@gmail.com>
2023-12-14 23:54:48 +0900

    add nvm config

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -163,6 +163,9 @@ export GIT_EDITOR=vim
 
 # node
 export PATH=$HOME/.nodebrew/current/bin:$PATH
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && \. "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 
 # mattermost
 ulimit -n 8096
